59519e66b4b282e476fe22f6cbf4ebc9bdecd59a
vishnuGangadharan <pvishnu997@gmail.com>
2024-10-10 12:10:02 +0530

    updated in db to atles

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,21 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 const url = process.env.DB_URL
-    const connectDB = async () => {
+console.log('urldddddddddf.......',url);
+
+
+const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(url)
-        if (connect) {
-            console.log("Database connected successfully");
-
-        } else {
-            console.log("Failed to connect database");
-
-        }
+      const connect =  await mongoose.connect(url)
+  
+      if (connect) {
+        console.log("Database connected successfully");
+      } else {
+        console.log("Failed to connect database");
+      }
     } catch (error) {
-        console.error(error.message);
-
+      console.error("DB connection error:", error.message);
     }
-}
-
+  }
+  
 export default connectDB
